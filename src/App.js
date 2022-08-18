c73b41f1edc121b00b5a2bf972bec19fc53e6415
WasinPanityayon <wasinpanityayon08@gmail.com>
2022-08-19 05:59:47 +0700

    submit 2

--- src/App.js
+++ src/App.js
@@ -31,9 +31,19 @@ var hint = hints[random];
 
 function App() {
   return (
-    <div>
-        <WordCard value={word}/>
-        <p>{hint}</p>
+    <div className='main'>
+      <h1>Guess the Word</h1>
+      <div className='content'>
+        <div className='word'>
+          <WordCard value={word}/>
+        </div>
+        <div className='details'>
+          <p className='hint'>Hint: <span>{hint}</span></p>
+          <p className='guess'>Remaining Guess: <span>10</span></p>
+          <p className='wrong'>Wrong letters: a, b, c</p>
+        </div>
+        <botton className='reset-btn'>Reset game</botton>
+      </div>
     </div>
   );
 }
